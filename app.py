8a5c212ca811e8357ead8253a0cacc0d273c459a
Lucky <luckysolanki902@gmail.com>
2023-10-24 13:27:00 +0530

    initial commit

--- app.py
+++ app.py
@@ -115,10 +115,5 @@ def registration():
 
     return render_template('registration.html', form=form)
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
+    app.run(debug=False)
